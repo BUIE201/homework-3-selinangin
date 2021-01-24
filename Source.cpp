a4eef1e0e7322291d44e8ac1815af8de7529824b
selinangin <selinangin2157@gmail.com>
2021-01-24 10:03:37 +0300

    binary tree

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,37 @@
 
 void main()
 {
+#include <iostream>
+
+using namespace std;
+
+struct Node
+{
+	int i;
+	Node* pLeft;
+	Node* pRight;
+
+	Node(int iIn) : i(iIn), pLeft(nullptr), pRight(nullptr) {}
+};
+
+void InsertToTree(Node*& pRoot, Node* pNew)
+{
+	if (!pRoot)
+	{
+		pRoot = pNew;
+		return;
+	}
+
+	if (pNew->i <= pRoot->i)
+		InsertToTree(pRoot->pLeft, pNew);
+	else
+		InsertToTree(pRoot->pRight, pNew);
+}
+
+
+
+
+
+
+
 }
